881b343a187b96d132aafd9f5f31041c729733e1
Barredafdez04 <barredafdez04@gmail.com>
2026-09-23 23:48:17 -0700

    Add files via upload

---
+++ operadores_aritmeticos.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let n1 = 10;
+    let n2 = 5;
+
+    println!("Sebastián Barreda Fernández");
+    println!(Numeros: {} y {}\n, n1, n2);
+
+    println!("Suma: {}", n1 + n2);
+    println!("Resta: {}", n1 - n2);
+    println!("Multiplicacion: {}", n1 * n2);
+    println!("División: {}", n1 / n2);
+    println!("Módulo: {}", n1 % n2);
+}
