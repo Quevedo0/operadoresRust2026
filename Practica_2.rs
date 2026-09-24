65f01ffb35eb499a65075a052e6a9ff19b5cb889
Leon <ryv9yzjz8c@privaterelay.appleid.com>
2026-09-23 22:20:11 -0700

    Add files via upload

---
+++ Practica_2.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+let a:f64 = 3.0;
+let b:f64 = 6.0;
+
+println!(" a = 3.0 \n");
+println!(" b = 6.0 \n");
+println!("\nOperadores arimeticos...\n");
+println!("a + b = {}", a + b);
+println!("a - b = {}", a - b);
+println!("a * b = {}", a * b);
+println!("a / b = {}", a / b);
+println!("a % b = {}", a % b);
+
+
+}
