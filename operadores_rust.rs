65510cf4db9d6061e5ba47c14354d8f339b18f1a
Bryan Yahir Aguirre Chavez <bryamaguirre541@gmail.com>
2026-09-23 17:24:52 -0700

    Add files via upload

---
+++ operadores_rust.rs
@@ -0,0 +1,12 @@
+fn main(){
+
+let a=2;
+let b=3;
+
+        println!("suma: a + b = {}",a+b);
+        println!("resta: a - b = {}",a-b);
+        println!("multiplicacion: a * b = {}",a*b);
+        println!("division: a / b = {}",a/b);
+        println!("modulo: a % b = {}",a%b);
+
+}
