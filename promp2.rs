38be4df2ea175d6271ab71ed8a608b654d3e2a68
Hopper501 <uysc2905@gmail.com>
2026-09-23 17:29:37 -0700

    Salazar Cazarez

---
+++ promp2.rs
@@ -0,0 +1,14 @@
+fn main(){
+//Salazar Cazarez Uziel Yeray
+
+let num = 15;
+let num2 = 10;
+
+println!("Números: ({}), ({})\n", num, num2);
+println!("Suma: {}", num + num2);
+println!("Resta: {}", num - num2);
+println!("Multiplicación: {}", num * num2);
+println!("División: {}", num / num2);
+println!("Módulo (residuo): {}", num % num2);
+
+}
