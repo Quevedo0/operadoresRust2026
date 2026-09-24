1b1b7749ecccb220c4ca250a4146ce01843fe70e
FranciscoBeltran65557 <juan.fbg2007@gmail.com>
2026-09-23 22:17:36 -0700

    Add files via upload

---
+++ prompt.rs
@@ -0,0 +1,25 @@
+fn main() {
+
+let x:f32 = 5.0;
+let y:f32 = 3.0;
+
+println!("Suma");
+let suma = x + y;
+println!("{x} + {y} = {suma}\n");
+
+println!("Resta");
+let resta = x - y;
+println!("{x} - {y} = {resta}\n");
+
+println!("Multiplicacion");
+let multi = x * y;
+println!("{x} x {y} = {multi}\n");
+
+println!("Division");
+let division = x / y;
+println!("{x} / {y} = {division}\n");
+
+println!("Modulo");
+let modulo = x % y;
+println!("{x} % {y} = {modulo}\n");
+}
