d42ab08c91a68f62aef6b1d40fb9b93be1f92381
Raul Duarte <raulenriqueduarte@gmail.com>
2026-09-23 18:19:57 -0700

    Entrega de trabajos en Rust - Raul Enrique Duarte Bojorquez

---
+++ Raul_Enrique_Duarte_Bojorquez/operadores_rust.rs
@@ -0,0 +1,16 @@
+fn main(){
+
+let n1 = 3;
+let n2 = 8;
+
+println!("suma: {}", n1 + n2);
+println!("resta: {}", n1 - n2);
+println!("multiplicación: {}", n1 * n2);
+println!("división: {}", n1 / n2);
+println!("módulo: {}", n1 % n2);
+
+
+}
+
+// Duarte Bojorquez Raul Enrique  2-1
+
---
+++ Raul_Enrique_Duarte_Bojorquez/promp2.rs
@@ -0,0 +1,16 @@
+fn main(){
+
+let n2 = 8;
+let n1 = 5;
+
+println!("modulo: {}", n1 % n2);
+println!("division: {}", n1 / n2);
+println!("multiplicacion: {}", n1 * n2);
+println!("resta: {}", n1 - n2);
+println!("suma: {}", n1 + n2);
+
+println!("numeros: {} y {}\n", n1, n2);
+
+}
+
+// Duarte Bojorquez Raul Enrique 2-1
